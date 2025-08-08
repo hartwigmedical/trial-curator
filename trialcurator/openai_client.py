72596ddb5cee1abdcea16c901682879673c80dbf
Junran Cao <j.cao@hartwigmedicalfoundation.nl>
2025-08-08 11:48:58 +1000

    trial-curator: Specify the exact LLM version (given the release of GPT-5)

--- trialcurator/openai_client.py
+++ trialcurator/openai_client.py
@@ -12,9 +12,8 @@ class OpenaiClient(LlmClient):
     OpenAI GPT models for generating text completions based on provided prompts.
     It extends the `LlmClient` abstract base class.
     """
-    MODEL = "gpt-4o"
-
-    # TO DO: Lock the model version when put into production. See "snapshots" section in  https://platform.openai.com/docs/models/gpt-4o
+    MODEL = "gpt-4o-2024-08-06"
+    # gpt-4o-2024-08-06 is the default gpt-4o. See https://platform.openai.com/docs/models/gpt-4o
 
     def __init__(self, temperature=0.0, top_p=1.0, model=MODEL):
         """
