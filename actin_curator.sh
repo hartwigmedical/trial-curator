c8bba400b798b3d64643573ae95294b0e3574696
Ned Leitch <ned.leitch@gmail.com>
2025-10-07 12:51:30 -0400

    Run only OpenAI for ACTIN
    Port in wrapper script from another branch which was about running both
    Gemini and OpenAI; modify it to only run OpenAI.
    
    Also update Dockerfile to fix environment variable references.

--- actin_curator.sh
+++ actin_curator.sh
@@ -15,7 +15,7 @@ die() {
 
 trap exit_handler EXIT
 
-[[ $# -eq 2 ]] || die "Provide the Docker image id and the local directory which will contain input/output"
+[[ $# -eq 2 || $# -eq 3 ]] || die "USAGE: $0 [Docker image id] [local directory to contain input/output] [OPTIONAL OpenAI key]"
 in_dir="$2/input"
 for dir in "$in_dir" "$2/output"; do
   mkdir -p $dir || die "Could not create/verify [$dir]"
@@ -23,7 +23,18 @@ done
 input="$in_dir/criteria.txt"
 [[ -f $input ]] || die "Cannot find input file [$input]"
 
-gc_home="${CLOUDSDK_CONFIG:-$(gcloud info --format='value(config.paths.global_config_dir)')}"
-[[ -n $gc_home ]] || die "Could not determine gcloud config directory! Export CLOUDSDK_CONFIG maybe."
-echo "Using gcloud configured in [$gc_home]"
-docker run --mount "type=bind,src=${2},dst=/actin_data" --mount "type=bind,src=${gc_home},dst=/root/.config/gcloud,ro" $1 
+BASE_CMD="docker run --mount type=bind,src=${2},dst=/actin_data"
+
+#gc_home="${CLOUDSDK_CONFIG:-$(gcloud info --format='value(config.paths.global_config_dir)')}"
+#[[ -n $gc_home ]] || die "Could not determine gcloud config directory! Export CLOUDSDK_CONFIG maybe."
+#echo "Using gcloud configured in [$gc_home]"
+#
+#echo "Running using Vertex"
+#$BASE_CMD --mount "type=bind,src=${gc_home},dst=/root/.config/gcloud,ro" $1
+
+if [[ $# -eq 3 ]]; then
+  echo "Running using OpenAI"
+  $BASE_CMD -e LLM_PROVIDER=OpenAI -e OPENAI_API_KEY="$3" $1
+else
+  echo "Not running with OpenAI as no API key was provided"
+fi
