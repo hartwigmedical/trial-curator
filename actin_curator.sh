f189e42f8b9889bbc525aa2c2ece739fe09ad0a6
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-11-10 14:25:02 +0100

    ACTIN-2745: Take downloaded file as input for ACTIN and update script

--- actin_curator.sh
+++ actin_curator.sh
@@ -15,26 +15,33 @@ die() {
 
 trap exit_handler EXIT
 
-[[ $# -eq 2 || $# -eq 3 ]] || die "USAGE: $0 [Docker image id] [local directory to contain input/output] [OPTIONAL OpenAI key]"
-in_dir="$2/input"
-for dir in "$in_dir" "$2/output"; do
-  mkdir -p $dir || die "Could not create/verify [$dir]"
+[[ $# -eq 1 ]] || die "USAGE: $0 [Docker image id]"
+docker_image_id=$1
+
+local_trial_curator_dir="$HOME/hmf/tmp/actin_data"
+in_dir="$local_trial_curator_dir/input"
+
+for dir in "$in_dir" "$local_trial_curator_dir/output"; do
+  mkdir -p $dir || die "Could not create or verify [$dir]"
 done
+
+trial_input_file="$HOME/Downloads/new_trial_paste_form.txt"
+if [[ -f $trial_input_file ]]; then
+  echo "Moving [$trial_input_file] to [$in_dir]..."
+  mv $trial_input_file $in_dir/criteria.txt
+  echo "Done!"
+else
+  echo "File [$trial_input_file] did not exist. Directly using input file in $in_dir (if any)."
+fi
+
 input="$in_dir/criteria.txt"
 [[ -f $input ]] || die "Cannot find input file [$input]"
 
-BASE_CMD="docker run --mount type=bind,src=${2},dst=/actin_data"
+openai_key_file="$HOME/hmf/hartwig_openai_key/key.txt"
+echo "Extracting OpenAi key from [$openai_key_file]. Be sure to use an ACTIN ORGANIZATIONAL OpenAi key!"
+[[ -f $openai_key_file ]] || die "Cannot find openai key [openai_key_file]. Be sure to add an ACTIN ORGANIZATIONAL OpenAi key!"
+openai_key=$(cat $openai_key_file)
 
-#gc_home="${CLOUDSDK_CONFIG:-$(gcloud info --format='value(config.paths.global_config_dir)')}"
-#[[ -n $gc_home ]] || die "Could not determine gcloud config directory! Export CLOUDSDK_CONFIG maybe."
-#echo "Using gcloud configured in [$gc_home]"
-#
-#echo "Running using Vertex"
-#$BASE_CMD --mount "type=bind,src=${gc_home},dst=/root/.config/gcloud,ro" $1
-
-if [[ $# -eq 3 ]]; then
-  echo "Running using OpenAI"
-  $BASE_CMD -e LLM_PROVIDER=OpenAI -e OPENAI_API_KEY="$3" $1
-else
-  echo "Not running with OpenAI as no API key was provided"
-fi
+BASE_CMD="docker run --mount type=bind,src=${local_trial_curator_dir},dst=/actin_data"
+echo "Running trial-curator for ACTIN using OpenAI"
+$BASE_CMD -e LLM_PROVIDER=OpenAI -e OPENAI_API_KEY=$openai_key $docker_image_id
