f7694c05327d5635063bbdc4d3947550ad0c04ff
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-11-11 11:21:34 +0100

    ACTIN-2745: PR

--- actin_curator.sh
+++ actin_curator.sh
@@ -29,27 +29,33 @@ for dir in "$in_dir" "$out_dir"; do
 done
 
 trial_input_file="$HOME/Downloads/new_trial_paste_form.txt"
-if [[ -f $trial_input_file ]]; then
-  info "Moving [$trial_input_file] to [$in_dir]..."
-  mv $trial_input_file $in_dir/criteria.txt
-  info "Done!"
-else
-  info "File [$trial_input_file] did not exist. Directly using input file in $in_dir (if any)."
-fi
+input_file="${in_dir}/criteria.txt"
 
-input="$in_dir/criteria.txt"
-[[ -f $input ]] || die "Cannot find input file [$input]"
+if [[ -f $trial_input_file ]]; then
+  info "Moving [$trial_input_file] to [$input_file]..."
+  mv $trial_input_file $input_file
+  info "Done!"
+elif [[ -f $input_file ]]; then
+  info "File [$trial_input_file] did not exist. Directly using input file [$input_file] to run trial-curator."
+else
+  die "Cannot find input file: [$trial_input_file] or [$input_file] both do not exist."
+fi
 
 openai_key_file="$HOME/hmf/hartwig_openai_key/key.txt"
 info "Extracting OpenAi key from [$openai_key_file]. Be sure to use an ACTIN ORGANIZATIONAL OpenAi key!"
-[[ -f $openai_key_file ]] || die "Cannot find openai key [openai_key_file]. Be sure to add an ACTIN ORGANIZATIONAL OpenAi key!"
+[[ -f $openai_key_file ]] || die "Cannot find openai key [$openai_key_file]. Be sure to add an ACTIN ORGANIZATIONAL OpenAi key!"
 openai_key=$(cat $openai_key_file)
 
 BASE_CMD="docker run --mount type=bind,src=${local_trial_curator_dir},dst=/actin_data"
 info "Running trial-curator for ACTIN using OpenAI"
 $BASE_CMD -e LLM_PROVIDER=OpenAI -e OPENAI_API_KEY=$openai_key $docker_image_id
-info "Done!"
 
-info "Reformat trial-curator output for inspection and use"
+if [ $? -eq 0 ]; then
+  info "Successfully ran Docker container!"
+else
+  die "Docker container run did not succeed. Exiting."
+fi
+
+info "Reformat trial-curator output for inspection and ACTIN use"
 jq -r '.[] | [.input_rule, .actin_rule_reformat] | @tsv' ${out_dir}/trial_curator_complete.out.OpenAI > ${out_dir}/trial_curator_complete.reformatted.tsv
-info "Done!"
+info "Everything done!"
