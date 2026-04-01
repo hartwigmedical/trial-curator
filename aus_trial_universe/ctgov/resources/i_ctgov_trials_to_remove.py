8975bbe1c87c4586920c52f1f92ef8d4d4dc473f
Junran Cao <j.cao@hartwigmedicalfoundation.nl>
2026-04-01 13:40:00 +1100

    aus_trial_universe: refactored ctgov workflow with better organisation

--- aus_trial_universe/ctgov/resources/i_ctgov_trials_to_remove.py
+++
@@ -1,4 +0,0 @@
-# These are non-cancer trials which got captured due to API_CONDITIONS_POTTR & which cannot be removed in the query without affecting legitimate POTTR trials
-
-trials_remove = ["NCT06370351", "NCT06461286", "NCT06970106"]
-
